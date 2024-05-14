ede392f789b98198afd5bb971116a55d1490d501
Namkha <namkha@Kunsangsssss-MacBook-Pro.local>
2024-05-14 19:27:14 +0545

    second

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+import json
+from django.http import JsonResponse,HttpResponse
+from django.forms.models import model_to_dict
+from products.models import Product
 
 # Create your views here.
-def api_home(request,*args,**kwargs):
-    return JsonResponse({"message":"Hi there,this is your django API response"})
+# def api_home(request,*args,**kwargs):
+#     print(request.GET)
+#     body = request.body
+#     data = {}
+#     try:
+#         data = json.loads(body)
+#     except:
+#         pass
+#     print(data.keys())
+#     data['params'] = dict(request.GET)
+#     data['headers'] = dict(request.headers)
+#     data['content_type'] = request.content_type
+#     return JsonResponse(data)
+
+# @api_view(["GET"])
+def api_home(request, *args, **kwargs):
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+        # data["title"] = model_data.title
+        # data["content"] = model_data.content
+        # data["price"] = model_data.price
+        #model instance (model_data)
+        #turn a python dict
+        #return Json to my client
+    return JsonResponse(data)
+    # return HttpResponse(json_data,headers={"content-type":"application/json"})
--- pyclient/basic.py
+++ pyclient/basic.py
@@ -4,6 +4,6 @@ import requests
 # endpoint =  "https://httpbin.org/status/200/"
 endpoint = "http://127.0.0.1:8000/api/"
 get_response = requests.get(endpoint)
-
-# Check if the request was successful (status code 200)
+# print(get_response.headers)
+# print(get_response.text)
 print(get_response.json())
